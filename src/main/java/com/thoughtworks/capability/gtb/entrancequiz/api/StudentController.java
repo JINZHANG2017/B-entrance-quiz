786dbff34ce4a8bc650ff140bc2b52965d9be6c5
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-10-14 14:28:48 +0800

    add gtb comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import com.thoughtworks.capability.gtb.entrancequiz.data.StudentRepo;
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
+// GTB: 无用的代码应该删除
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +14,17 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 public class StudentController {
+    // GTB: 违反Restful实践, url不合理
+    // GTB: ResponseEntity应该使用泛型
     @GetMapping("/student/list")
     public ResponseEntity<List<Student>> getStudentList(@RequestParam Integer isShuffle){
+        // GTB: 无用的代码应该删除
 //        List<String> list=new ArrayList<>();
 //        list.add("hello");
+        // GTB: 代码风格问题，等号两边要加空格，很多该加空格的地方都没加空格
+        // GTB: StudentRepo实例应该被注入进来，而非自己new
         StudentRepo studentRepo=new StudentRepo();
+        // GTB: 无用的初始值
         List<Student> studentList=new ArrayList<>();
         if(isShuffle!=null&&isShuffle==1){
             studentList=studentRepo.getRandomStudentList();
@@ -39,11 +46,17 @@ public class StudentController {
     }
 
     @PostMapping("/student/list")
+    // GTB: 应该定义对象来接收RequestBody
+    // GTB: Post请求应该返回201
+    // GTB: 方法名不合理，名不副实
     public ResponseEntity getStudentList(@RequestBody Map studentNameMap){
 //        List<String> list=new ArrayList<>();
 //        list.add("hello");
         StudentRepo studentRepo=new StudentRepo();
         List<Student> studentList=studentRepo.getStudentList();
+        // GTB: “studentList.size() + 1”不可读，建议引入中间变量
+        // GTB: 保存数据的操作应该封装进Repository
+        // GTB: 不符合三层架构，缺少Service层来处理业务逻辑
         studentList.add(new Student(studentList.size()+1,studentNameMap.get("name").toString()));
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/data/StudentRepo.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/data/StudentRepo.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+// GTB: Repo建议写全拼
+// GTB: StudentRepo不是一个合格的Repository层
 public class StudentRepo {
     private static final String ORIGIN_STR="成吉思汗,鲁班七号,太乙真人,钟无艳,花木兰,雅典娜,芈月,白起,刘禅,庄周,马超,刘备,哪吒,大乔,蔡文姬";
     public List<Student> getStudentList(){
@@ -14,6 +16,7 @@ public class StudentRepo {
     }
     private static List<Student> list=new ArrayList<>();
     static {
+        // GTB: 可以使用Java8 Stream API简化
         String[] strArray=ORIGIN_STR.split(",");
         for(int i=0;i<strArray.length;i++){
             list.add(new Student(i+1,strArray[i]));
